464396253a9a577b025fc30e1935104a55675116
Khatiwada <bidhan.khatiwada@cotiviti.com>
2023-07-31 17:14:00 +0545

    Frontend issue fix

--- vemployee/src/main/java/com/cotiviti/vemployee/dto/Employee.java
+++ vemployee/src/main/java/com/cotiviti/vemployee/dto/Employee.java
@@ -1,17 +1,21 @@
-//package com.cotiviti.vemployee.dto;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//@Getter
-//@Setter
-//public class Employee {
-//    private String name;
-//    private String designation;
-//    private String email;
-//    private String contact;
-//    private String iNumber;
-//    private String mobileNumber;
-//    private String skypeId;
-//    private boolean hasSubordinates;
-//}
+package com.cotiviti.vemployee.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Employee {
+    private String name;
+    private String designation;
+    private String email;
+    private String contact;
+    private String iNumber;
+    private String mobileNumber;
+    private String skypeId;
+    private List<Employee> subordinates;
+    private Employee manager;
+
+}
